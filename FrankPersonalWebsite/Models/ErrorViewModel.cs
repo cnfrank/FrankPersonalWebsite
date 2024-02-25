03288aa074d4347fe153c96c960927d63427545f
FrankZhou <9570075@qq.com>
2024-02-25 14:21:18 +1300

    .net core mvc init

---
+++ FrankPersonalWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FrankPersonalWebsite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
